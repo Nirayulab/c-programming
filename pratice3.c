7d1a021851eb121e9b8a266334b116c033fbb6f3
Nirayulab <81407593+Nirayulab@users.noreply.github.com>
2021-05-08 13:22:03 +0545

    Update pratice3.c

--- pratice3.c
+++ pratice3.c
@@ -1,3 +1,4 @@
+//write a program to find out whether a student is pass or fail, if it requires total 40% and at least 33%in each student to pass. aasume 3 subject and take marks as an input from the user//
 #include <stdio.h>
 
 int main(){
